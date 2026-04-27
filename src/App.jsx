003f61722b5044de2250524f3b952a8427fafcad
Gailcapulong <gailcapulong77@gmail.com>
2026-04-28 00:02:37 +0800

    fix: update hero background image path to use local image

--- src/App.jsx
+++ src/App.jsx
@@ -91,6 +91,8 @@ const App = () => {
             <div className="pattern" />
 
             <div className="wrapper" >
+               
+             <img src="./hero.png" alt="Hero Banner" />
             <header>
                 <h1> Find <span className="text-gradient">Movies</ span> You'll Enjoy without the Hassle</h1>
             </header>
